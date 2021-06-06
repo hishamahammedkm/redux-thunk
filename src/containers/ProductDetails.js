0790c3a0959cc5d80358dcad15e2b9947e52209f
hishamahammedkm <hishamahammedkm@gmail.com>
2021-06-06 17:15:56 +0530

    first commit

--- src/containers/ProductDetails.js
+++ src/containers/ProductDetails.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  selectedProduct,
+  fetchProduct,
   removeSelectedProduct,
 } from "../redux/actions/productsActions";
 const ProductDetails = () => {
@@ -11,17 +11,17 @@ const ProductDetails = () => {
   let product = useSelector((state) => state.product);
   const { image, title, price, category, description } = product;
   const dispatch = useDispatch();
-  const fetchProductDetail = async (id) => {
-    const response = await axios
-      .get(`https://fakestoreapi.com/products/${id}`)
-      .catch((err) => {
-        console.log("Err: ", err);
-      });
-    dispatch(selectedProduct(response.data));
-  };
+  // const fetchProductDetail = async (id) => {
+  //   const response = await axios
+  //     .get(`https://fakestoreapi.com/products/${id}`)
+  //     .catch((err) => {
+  //       console.log("Err: ", err);
+  //     });
+  //   dispatch(selectedProduct(response.data));
+  // };
 
   useEffect(() => {
-    if (productId && productId !== "") fetchProductDetail(productId);
+    if (productId && productId !== "") dispatch(fetchProduct(productId))
     return () => {
       dispatch(removeSelectedProduct());
     };
